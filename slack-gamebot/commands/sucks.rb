4ca69614f4bd17768f97d2fa6f5ac29650c635f6
Marin Hyatt <marin.hyatt@gmail.com>
2017-07-12 21:49:13 -0400

    Testing changes

--- slack-gamebot/commands/sucks.rb
+++ slack-gamebot/commands/sucks.rb
@@ -4,11 +4,11 @@ module SlackGamebot
       command 'sucks', 'suck', 'you suck', 'sucks!', 'you suck!' do |client, data, _match|
         user = ::User.find_create_or_update_by_slack_id!(client, data.user)
         if user.losses && user.losses > 5
-          client.say(channel: data.channel, text: "No <@#{data.user}>, with #{user.losses} losses, you suck!", gif: 'loser')
+          client.say(channel: data.channel, text: "No <@#{data.user}>, with #{user.losses} losses, you rock!", gif: 'loser')
         elsif user.rank && user.rank > 3
-          client.say(channel: data.channel, text: "No <@#{data.user}>, with a rank of #{user.rank}, you suck!", gif: 'loser')
+          client.say(channel: data.channel, text: "No <@#{data.user}>, with a rank of #{user.rank}, you rock!", gif: 'loser')
         else
-          client.say(channel: data.channel, text: "No <@#{data.user}>, you suck!", gif: 'rude')
+          client.say(channel: data.channel, text: "No <@#{data.user}>, you rock!", gif: 'rude')
         end
         logger.info "SUCKS: #{client.owner} - #{data.user}"
       end
